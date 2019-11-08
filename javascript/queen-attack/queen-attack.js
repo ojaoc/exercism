08c487d26bdcc0449dafcb9b1b9ebb72bbbbf1b5
ojaoc <correia.jafonso@gmail.com>
2019-11-08 14:59:26 +0000

    queen first commit

--- javascript/queen-attack/queen-attack.js
+++ javascript/queen-attack/queen-attack.js
@@ -3,16 +3,30 @@
 // convenience to get you started writing code faster.
 //
 
-export class QueenAttack {
-    constructor() {
-        throw new Error("Remove this statement and implement this function");
-    }
+// export class QueenAttack {
+//     constructor() {
+//         throw new Error("Remove this statement and implement this function");
+//     }
 
-    toString() {
-        throw new Error("Remove this statement and implement this function");
-    }
+//     toString() {
+//         throw new Error("Remove this statement and implement this function");
+//     }
 
-    canAttack() {
-        throw new Error("Remove this statement and implement this function");
+//     canAttack() {
+//         throw new Error("Remove this statement and implement this function");
+//     }
+// }
+let grid = [];
+let column = 0;
+let emptyString = '';
+for (let i = 0; i < 8; i++) {
+    while (column < 8) {
+        emptyString += ` [${i}, ${column}]`;
+        column++;
     }
+    grid.push(emptyString.slice(1));
+    emptyString = '';
+    column = 0;
 }
+
+console.log(grid)
