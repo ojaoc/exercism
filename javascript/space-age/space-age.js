75c3f96e3da811e8f5421ee0048daf4e312a1723
ojaoc <correia.jafonso@gmail.com>
2019-11-11 11:46:05 +0000

    replaced switch statement with object for fewer lines of code

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,45 +1,25 @@
 export const age = (planet, ageInSeconds) => {
-  let orbitalPeriod; //in Earth years!
   const EARTH_ORBITAL_SECONDS = 31557600;
+  const ORBITAL_PERIOD = {
+    mercury: 0.2408467,
 
-  switch (planet) {
-    case 'mercury':
-      orbitalPeriod = 0.2408467;
-      break;
+    venus: 0.61519726,
 
-    case 'venus':
-      orbitalPeriod = 0.61519726;
-      break;
+    earth: 1,
 
-    case 'earth':
-      orbitalPeriod = 1;
-      break;
+    mars: 1.8808158,
 
-    case 'mars':
-      orbitalPeriod = 1.8808158;
-      break;
+    jupiter: 11.862615,
 
-    case 'jupiter':
-      orbitalPeriod = 11.862615;
-      break;
+    saturn: 29.447498,
 
-    case 'saturn':
-      orbitalPeriod = 29.447498;
-      break;
+    uranus: 84.016846,
 
-    case 'uranus':
-      orbitalPeriod = 84.016846;
-      break;
-
-    case 'neptune':
-      orbitalPeriod = 164.79132;
-      break;
-
-    default:
-      throw new Error("Please enter a valid planet. Pluto is not a planet.");
+    neptune: 164.79132
   }
 
-  const RESULT = ageInSeconds / (orbitalPeriod * EARTH_ORBITAL_SECONDS);
+  const RESULT = ageInSeconds / (ORBITAL_PERIOD[planet] * EARTH_ORBITAL_SECONDS);
+
   return Math.round(RESULT * 100) / 100;
   // This will return the age in Earth years, rounded to two decimal places. 
 };
