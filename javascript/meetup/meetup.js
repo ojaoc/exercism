32bb537a2c1fffac510e47a5300493502aac23c6
ojaoc <correia.jafonso@gmail.com>
2019-11-14 21:50:50 +0000

    exercise complete with all tests passed

--- javascript/meetup/meetup.js
+++ javascript/meetup/meetup.js
@@ -1,8 +1,59 @@
-//
-// This is only a SKELETON file for the 'Meetup' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const meetupDay = (year, month, dayOfTheWeek, descriptor) => {
+  let date = new Date(year, month);
 
-export const meetupDay = (day) =>  {
-  throw new Error("Remove this statement and implement this function");
+  const WEEKDAYS = [
+    'sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday'
+  ];
+  const ORDER = [
+    '1st',
+    '2nd',
+    '3rd',
+    '4th',
+    '5th',
+  ];
+
+  let sameWeekDayInMonth = [];
+  let day;
+
+  for (let i = 1; i < 32; i++) {
+    date.setDate(i);
+    /* In the if statement below, I had to check if the day of the
+    week of the iterated day was the same as the argument dayOfTheWeek.
+    I also had to check whether or not the iterated day was from the corresponding month argument, because there are months with less
+    than 31 days. */
+    if (dayOfTheWeek.toLowerCase() === WEEKDAYS[date.getDay()] &&
+      date.getMonth() === month) {
+      sameWeekDayInMonth.push(date.getDate());
+    } else {
+      continue;
+    }
+  }
+
+  const isTeenth = (int) => {
+    if (int > 12 && int < 20) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  if (descriptor === 'teenth') {
+    day = sameWeekDayInMonth.find(isTeenth);
+  } else if (descriptor === 'last') {
+    day = sameWeekDayInMonth.slice(-1)[0];
+  } else if (ORDER.includes(descriptor)) {
+    if (sameWeekDayInMonth[ORDER.indexOf(descriptor)] === undefined) {
+      throw this;
+    } else {
+      day = sameWeekDayInMonth[ORDER.indexOf(descriptor)];
+    }
+  }
+
+  return date = new Date(year, month, day);
 }
