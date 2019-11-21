f2781e610cb2b91cb013cfd2bbc3cd9bc5ffb110
ojaoc <correia.jafonso@gmail.com>
2019-11-21 18:57:37 +0000

    continue this later

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,18 +1,23 @@
-//
-// This is only a SKELETON file for the 'Matrix' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export class Matrix {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+class Matrix {
+  constructor(matrix) {
+    this.matrix = matrix
+      .split("\n");
+      .map(row => {
+        for (let i = 0; i < row.length; i++) {
+          if (parseInt(row.charAt(i)))
+          parseInt(row.charAt(i));
+        }
+      })
   }
 
   get rows() {
-    throw new Error("Remove this statement and implement this function");
+    return this.matrix;
   }
 
   get columns() {
     throw new Error("Remove this statement and implement this function");
   }
 }
+
+let matrix = new Matrix('1 2\n3 4');
+console.log(matrix.rows);
