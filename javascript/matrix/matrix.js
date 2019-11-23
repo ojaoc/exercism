462c057008989b5f0ed6850b6fef1eadb2be9f38
ojaoc <correia.jafonso@gmail.com>
2019-11-23 00:28:52 +0000

    Exercise completed all tests passed

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,29 +1,34 @@
-class Matrix {
+export class Matrix {
   constructor(string) {
     this.matrix = string
       .split("\n"); // Seperate elements by row 
-    this.row = [];
-    let columns = [];
+    this.listOfRows = [];
+    this.listOfColumns = [];
     /* The code below will turn each string number into 
     a real number. Now, we have an array of rows, each 
     containing the corresponding integers */
     for (let i = 0; i < this.matrix.length; i++) {
-      this.row.push(this.matrix[i]
+      this.listOfRows.push(this.matrix[i]
         .match(/\d+/g)
         .map(item => {
           return Number(item);
         }));
     }
+
+    for (let n = 0; n < this.listOfRows[0].length; n++) {
+      let eachColumn = [];
+      this.listOfRows.forEach(item => {
+        eachColumn.push(item[n]);
+      });
+      this.listOfColumns.push(eachColumn);
+    }
   }
 
   get rows() {
-    return this.row;
+    return this.listOfRows;
   }
 
   get columns() {
-    // for (let i = 0; i < this)
+    return this.listOfColumns;
   }
 }
-
-let matrix = new Matrix('1 3 4 3\n3 2 1 7');
-console.log(matrix.rows[2]);
