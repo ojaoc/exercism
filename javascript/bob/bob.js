22bb657a643eb807efca5ff9ab309600bdcd4a73
ojaoc <correia.jafonso@gmail.com>
2019-12-10 11:55:58 +0000

    Put helper functions outside of main function

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,32 +1,48 @@
+const isSilence = message => message.trim().length === 0;
+
+const isQuestion = message => message.slice(-1) === '?';
+
+const isYelling = message => {
+
+  message.split('');
+  
+  for (let i = 0; i < message.length; i++) {
+  
+    if (/[A-Z]/.test(message[i]) && /[A-Z]/.test(message[i+1])) {
+
+      if (message[i] + message[i+1] === 'OK') {
+
+        continue;
+
+      } else if (message[i] + message[i+1] + message[i+2] === 'DMV') {
+
+        return false;
+
+      }
+
+      return true;
+
+    } 
+  }  
+
+  return false;
+
+}
+
 export const hey = (message) => {
+  
   message = message.trim();
 
-  const IS_SILENCE = message.length === 0;
-  if (IS_SILENCE) return 'Fine. Be that way!'; // Saying nothing
+  if (isSilence(message)) return 'Fine. Be that way!'; // Saying nothing
 
-  const IS_QUESTION = message.slice(-1) === '?';
+  if (isQuestion(message)) {
 
-  const IS_YELLING = () => {
-    message = message.split('');
+    return isYelling(message) ? 'Calm down, I know what I\'m doing!' : 'Sure.';
 
-    for (let i = 0; i < message.length; i++) {
-  
-      if (/[A-Z]/.test(message[i]) && /[A-Z]/.test(message[i+1])) {
-  
-        if (message[i] + message[i+1] === 'OK') {
-          continue;
-        } else if (message[i] + message[i+1] + message[i+2] === 'DMV') {
-          break;
-        }
+  } else {
 
-        return true;
-      } 
-    }  
-  }
-  
-  if (IS_QUESTION) {
-    return IS_YELLING() ? 'Calm down, I know what I\'m doing!' : 'Sure.';
+    return isYelling(message) ? 'Whoa, chill out!' : 'Whatever.';
+
   }
 
-  return IS_YELLING() ? 'Whoa, chill out!' : 'Whatever.';
 };
